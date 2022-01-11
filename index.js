4f5e709119a8f23481514938feeb0c2a154088cd
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-01-11 19:15:37 +0100

    dotenv heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.port || 5000;
 const productsApi = require ('./controllers/productApi');
 const cors = require('cors');
 const path = require('path');
